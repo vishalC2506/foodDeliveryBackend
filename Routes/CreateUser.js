e28d147078cb4dbc182b112f59143c8bba4d1516
vishal kumar chaudhary <chaudharyv2506@gmail.com>
2023-12-20 23:41:18 +0530

    retry

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -67,8 +67,6 @@ router.post(
       const authToken = jwt.sign(data, SecretKey);
       return res.json({ success: true, authToken: authToken });
     } catch (err) {
-      console.log(err);
-      res.json(err)
       res.json({ success: false });
     }
   }
