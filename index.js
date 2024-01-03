211666e59a12cf941f71a6a483f0d393eca708b8
vishal kumar chaudhary <chaudharyv2506@gmail.com>
2024-01-03 21:17:06 +0530

    error commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express");
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT||4000 ;
 const cors = require("cors");
 const mongoDB = require("./db");
 mongoDB();
